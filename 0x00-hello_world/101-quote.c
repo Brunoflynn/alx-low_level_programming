04d60af314d77a3e70b362520572b5435f17ed5e
Brunoflynn <flynnbruno4@gmail.com>
2023-03-16 21:47:21 -0700

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that prints a like to a standard error
- * Return 1 (success)
+ * Return: 1 when successful
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1)
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
