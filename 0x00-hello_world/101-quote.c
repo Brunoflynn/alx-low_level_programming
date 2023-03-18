19ec058c31a20fb887e3cce082b6b0876c70f03c
Brunoflynn <flynnbruno4@gmail.com>
2023-03-18 07:51:22 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - A c program that prints a like to a standard error
+ * main - A C program that prints a line to the standard error
+ *
  * Return: 1 when successful
  */
-
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		stderr);
 	return (1);
 }
+
