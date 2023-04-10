4d965a912f2a0eeddc8520689cb64f00bf5ea4ba
Brunoflynn <flynnbruno4@gmail.com>
2023-04-10 04:06:52 -0700

    library

---
+++ 0x09-static_libraries/0x09-static_libraries/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Outputs a string to the standard output stream.
+ *
+ * @str: String to output.
+ *
+ * Return: None.
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("Hello, world!");
+	return (0);
+}
